7ee3a63358db69cc108d2b90c6ebb69024ca1d33
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-21 21:40:16 +0900

    submitイベントの開始

--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,22 @@
 import { EventListener } from './EventListener';
 
 class Application {
+  private readonly eventListener = new EventListener();
+
   start() {
-    const eventListener = new EventListener();
+    const createForm = document.getElementById('createForm') as HTMLElement;
 
-    const button = document.getElementById('deleteAllDoneTask');
+    this.eventListener.add(
+      'submit-handler',
+      'submit',
+      createForm,
+      this.handleSubmit
+    );
+  }
 
-    if (!button) return;
-
-    eventListener.add('sample', 'click', button, () => alert('clicked'));
-
-    eventListener.remove('sample');
+  handleSubmit(e: Event) {
+    e.preventDefault();
+    console.log('submit');
   }
 }
 
