126bf9e53d86fbcb0445a849c53549404bb10c0e
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-23 21:27:32 +0900

    アプリ起動時のデータ取得

--- ts/TaskCollection.ts
+++ ts/TaskCollection.ts
@@ -3,12 +3,12 @@ import { Status, Task } from './Task';
 const STORAGE_KEY = 'TASKS';
 
 export class TaskCollection {
-  private tasks: Task[] = [];
   private readonly storage;
+  private tasks;
 
   constructor() {
     this.storage = localStorage;
-    console.log(this.storage);
+    this.tasks = this.getStoredTasks();
   }
 
   add(task: Task) {
@@ -38,5 +38,22 @@ export class TaskCollection {
 
   private updateStorage() {
     this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+  }
+
+  private getStoredTasks() {
+    const jsonString = this.storage.getItem(STORAGE_KEY);
+
+    if (!jsonString) return [];
+
+    try {
+      const storedTasks: any[] = JSON.parse(jsonString);
+      const tasks = storedTasks.map((task) => new Task(task));
+
+      console.log(tasks);
+      return tasks;
+    } catch {
+      this.storage.removeItem(STORAGE_KEY);
+      return [];
+    }
   }
 }
